b6083ab7d22ca2ad0ebd5293f8949754c79ca3c9
Yoobin JO <dbqlsdl1234@naver.com>
2020-07-20 00:27:54 +0900

    Union-Find 알고리즘

--- AlgorithmC++/Algorithm/UnionFindAlgorithm/UnionFind.cpp
+++ AlgorithmC++/Algorithm/UnionFindAlgorithm/UnionFind.cpp
@@ -7,3 +7,48 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int getParent(int parent[], int x) {
+    if(parent[x] == x) return x;
+    return parent[x] = getParent(parent, parent[x]);
+}
+
+void unionParent(int parent[], int a, int b) {
+    a = getParent(parent, a);
+    b = getParent(parent, b);
+    if(a < b) parent[b] = a;
+    else parent[a] = b;
+}
+
+int findParend(int parent[], int a, int b) {
+    a = getParent(parent, a);
+    b = getParent(parent, b);
+    if(a == b) return 1;
+    return 0;
+}
+
+int main() {
+    int parent[11];
+    for(int i = 1; i < 11; i++) {
+        parent[i] = i;
+    }
+    
+    unionParent(parent, 1, 2);
+    unionParent(parent, 2, 3);
+    unionParent(parent, 3, 4);
+//    unionParent(parent, 4, 5);
+    
+    unionParent(parent, 5, 8);
+    unionParent(parent, 6, 7);
+    unionParent(parent, 7, 8);
+    unionParent(parent, 2, 7);
+    
+//    for(int i = 0; i < 11; i++) {
+//
+//    }
+    
+    return 0;
+}
