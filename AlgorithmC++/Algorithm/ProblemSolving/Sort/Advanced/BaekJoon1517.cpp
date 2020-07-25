05fce854e4dd4f5310619cea51455d7b04035799
Yoobin JO <dbqlsdl1234@naver.com>
2020-07-26 00:51:05 +0900

    백준 1517 다시 풀어봄

--- AlgorithmC++/Algorithm/ProblemSolving/Sort/Advanced/BaekJoon1517.cpp
+++ AlgorithmC++/Algorithm/ProblemSolving/Sort/Advanced/BaekJoon1517.cpp
@@ -55,21 +55,35 @@ void merge(int m, int middle, int n) {
     int k = m;
     int cnt = 0;
     
-    while(i <= middle || j <= n) {
-        if(i > middle) {
-            tmp[k++] = v[j++];
-            cnt++;
-        } else if(j > n) {
+//    while(i <= middle || j <= n) {
+//        if(i > middle) {
+//            tmp[k++] = v[j++];
+//            cnt++;
+//        } else if(j > n) {
+//            tmp[k++] = v[i++];
+//            ans += (long long)cnt;
+//        } else if(v[i] <= v[j]) {
+//            tmp[k++] = v[i++];
+//            ans += (long long)cnt;
+//        } else {
+//            tmp[k++] = v[j++];
+//            cnt++;
+//        }
+//    }
+    
+    while(i <= middle && j <= n) {
+        if(v[i] <= v[j])
             tmp[k++] = v[i++];
-            ans += (long long)cnt;
-        } else if(v[i] <= v[j]) {
-            tmp[k++] = v[i++];
-            ans += (long long)cnt;
-        } else {
+        else {
             tmp[k++] = v[j++];
-            cnt++;
+            ans += middle + 1 - i;
         }
     }
+    while(i <= middle)
+        tmp[k++] = v[i++];
+    while(j <= n)
+        tmp[k++] = v[j++];
+    
     for(i = m; i <= n; i++)
         v[i] = tmp[i];
 }
