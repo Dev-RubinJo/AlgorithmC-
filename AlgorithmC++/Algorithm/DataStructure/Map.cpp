74b6c38561bf312667d340e4c16540ad9fd0c051
Rubin.Jo <dbqlsdl1234@naver.com>
2020-08-06 00:29:31 +0900

    Map STL

---
+++ AlgorithmC++/Algorithm/DataStructure/Map.cpp
@@ -0,0 +1,35 @@
+//
+//  Map.cpp
+//  Algorithm
+//
+//  Created by YooBin Jo on 2020/08/05.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+#include <stdio.h>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    vector<pair<int, int> > v;
+    
+    map<int, int> temp;
+    
+    temp.insert(make_pair(1, 3));
+    temp.insert(make_pair(2, 2));
+    
+    temp.insert(make_pair(6, 2));
+    temp.insert(make_pair(3, 2));
+    
+    
+    
+    // key를 기준으로 자동 정렬 (오름차순)
+    
+    
+    
+    
+    
+    return 0;
+}
