e6cb5d8ed081c05cc69034cdd1cf4a9a91562e02
Rubin.Jo <dbqlsdl1234@naver.com>
2020-08-10 23:52:03 +0900

    백준 1439 해결

--- AlgorithmC++/Algorithm/ProblemSolving/Greedy/BaekJoon1439.cpp
+++ AlgorithmC++/Algorithm/ProblemSolving/Greedy/BaekJoon1439.cpp
@@ -12,7 +12,19 @@
 using namespace std;
 
 int main(void) {
+    int one = 0, zero = 0;
+    string str;
+    cin >> str;
     
+    if(str[0] == '0') zero += 1;
+    else one += 1;
     
+    for(int i = 0; i < str.size() - 1; i++) {
+        if(str[i] != str[i + 1]) {
+            if(str[i + 1] == '1') one += 1;
+            else zero += 1;
+        }
+    }
+    cout << min(zero, one);
     return 0;
 }
